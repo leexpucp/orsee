94668cfc836f65f147dc0f1929f9ee586e0a7b2d
Carlo1911 <carlo.alva@pucp.pe>
2017-08-10 23:35:02 -0500

    prueba termns

--- public/terms_conditions.php
+++ public/terms_conditions.php
@@ -14,9 +14,6 @@ if ($proceed) {
             if (isset($_REQUEST['accept_rules']) && $_REQUEST['accept_rules']) {
                 $_SESSION['rules']=true;
                 redirect ("public/".thisdoc());
-            } elseif (isset($_REQUEST['notaccept_rules']) && $_REQUEST['notaccept_rules']) {
-                unset ($_SESSION['subpool_id']);
-                redirect ("public/");
             } else {
                 echo '<center><BR><BR>
                       <FORM action='.thisdoc().'>
@@ -63,94 +60,6 @@ if ($proceed) {
         $proceed=false;
     }
 }
-
-// if ($proceed) {
-//     echo '<center>';
-//     $form=true; $errors__dataform=array();
-//     if (isset($_REQUEST['add'])) {
-//         $continue=true;
-
-//         if (!isset($_REQUEST['captcha']) || !isset($_SESSION['captcha_string']) || $_REQUEST['captcha']!=$_SESSION['captcha_string']) {
-//             if (!isset($_REQUEST['subscriptions']) || !is_array($_REQUEST['subscriptions'])) $_REQUEST['subscriptions']=array();
-//             $_REQUEST['subscriptions']=id_array_to_db_string($_REQUEST['subscriptions']);
-//             $continue=false;
-//             message(lang('error_wrong_captcha'));
-//         }
-
-//         if ($continue) {
-//             // checks and errors
-//             foreach ($_REQUEST as $k=>$v) {
-//                 if(!is_array($v)) $_REQUEST[$k]=trim($v);
-//             }
-//             $_REQUEST['subpool_id']=$_SESSION['subpool_id'];
-//             $errors__dataform=participantform__check_fields($_REQUEST,false);
-//             $error_count=count($errors__dataform);
-//             if ($error_count>0) $continue=false;
-
-//             $response=participantform__check_unique($_REQUEST,"create");
-//             if (isset($response['disable_form']) && $response['disable_form']) {
-//                 $continue=false;
-//                 $proceed=false;
-//                 unset ($_SESSION['subpool_id']);
-//                 unset ($_SESSION['rules']);
-//                 unset ($_SESSION['pauthdata']['pw_provided']);
-//                 unset ($_SESSION['pauthdata']['submitted_checked_pw']);
-//                 if($settings['subject_authentication']=='token') {
-//                     redirect ("public/");
-//                 } else {
-//                     redirect ("public/participant_login.php");
-//                 }
-//             } elseif($response['problem']) {
-//                 $continue=false;
-//             }
-
-//             if ($settings['subject_authentication']!='token') {
-//                 if (isset($_SESSION['pauthdata']['pw_provided']) && $_SESSION['pauthdata']['pw_provided'] &&
-//                     isset($_SESSION['pauthdata']['submitted_checked_pw']) && $_SESSION['pauthdata']['submitted_checked_pw']) {
-//                         $_REQUEST['password']=$_SESSION['pauthdata']['submitted_checked_pw'];
-//                 } else {
-//                     $pw_ok=participant__check_password($_REQUEST['password'],$_REQUEST['password2']);
-//                     if ($pw_ok) {
-//                         $_SESSION['pauthdata']['pw_provided']=true;
-//                         $_SESSION['pauthdata']['submitted_checked_pw']=$_REQUEST['password'];
-//                     } else {
-//                         $continue=false;
-//                     }
-//                 }
-//             }
-//         }
-
-//         if ($continue) {
-//             $participant=$_REQUEST;
-//             unset ($_SESSION['pauthdata']['pw_provided']);
-//             unset ($_SESSION['pauthdata']['submitted_checked_pw']);
-//             unset ($_SESSION['captcha_string']);
-//             $new_id=participant__create_participant_id($participant);
-//             $participant['participant_id']=$new_id['participant_id'];
-//             $participant['participant_id_crypt']=$new_id['participant_id_crypt'];
-//             if ($settings['subject_authentication']!='token') {
-//                 $participant['password_crypted']=unix_crypt($participant['password']);
-//             }
-//             $participant['confirmation_token']=create_random_token(get_entropy($participant));
-//             $participant['creation_time']=time();
-//             $participant['last_profile_update']=$participant['creation_time'];
-//             $participant['status_id']=0;
-//             $participant['subpool_id']=$_SESSION['subpool_id'];
-//             if (!isset($participant['language']) || !$participant['language']) $participant['language']=$settings['public_standard_language'];
-//             $done=orsee_db_save_array($participant,"participants",$participant['participant_id'],"participant_id");
-//             if ($done) {
-//                 log__participant("subscribe",$participant['lname'].', '.$participant['fname']);
-//                 $proceed=false;
-//                 $done=experimentmail__confirmation_mail($participant);
-//                 message(lang('successfully_registered'));
-//                 redirect ("public/");
-//             } else {
-//                 message(lang('database_error'));
-//             }
-//         }
-//     }
-// }
-
 
 include("footer.php");
 ?>
