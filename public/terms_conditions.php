7b8b08aed904c81875513569477189c46f9b46c5
Carlo1911 <carlo.alva@pucp.pe>
2017-08-10 23:42:12 -0500

    prueba termns

--- public/terms_conditions.php
+++ public/terms_conditions.php
@@ -11,34 +11,30 @@ if ($proceed) {
     if (!isset($_SESSION['rules'])) {
         if ($settings['registration__require_rules_acceptance']=='y' ||
             $settings['registration__require_privacy_policy_acceptance']=='y') {
-            if (isset($_REQUEST['accept_rules']) && $_REQUEST['accept_rules']) {
-                $_SESSION['rules']=true;
-                redirect ("public/".thisdoc());
-            } else {
-                echo '
-                      <FORM action='.thisdoc().'>
-                      <TABLE class="or_panel" style="width: 80%">';
-                if ($settings['registration__require_rules_acceptance']=='y') {
-                    echo '<TR><TD>
-                    <TABLE width="100%" border=0 class="or_panel_title"><TR>
-                        <TD style="background: '.$color['panel_title_background'].'; color: '.$color['panel_title_textcolor'].'">
-                            '.lang('rules').'
-                        </TD>
-                    </TR></TABLE>
-                    </TD></TR>
-                        <TR><TD>'.content__get_content("rules").'</TD></TR>';
-                }
-                if ($settings['registration__require_privacy_policy_acceptance']=='y') {
-                    echo '<TR><TD>
-                        <TABLE width="100%" border=0 class="or_panel_title"><TR>
-                        <TD style="background: '.$color['panel_title_background'].'; color: '.$color['panel_title_textcolor'].'">
-                            '.lang('privacy_policy').'
-                        </TD>
-                        </TR></TABLE>
-                    </TD></TR>
-                            <TR><TD>'.content__get_content("privacy_policy").'</TD></TR>';
-                }
+            echo '<center><BR><BR>
+                    <FORM action='.thisdoc().'>
+                    <TABLE class="or_panel" style="width: 80%">';
+            if ($settings['registration__require_rules_acceptance']=='y') {
+                echo '<TR><TD>
+                <TABLE width="100%" border=0 class="or_panel_title"><TR>
+                    <TD style="background: '.$color['panel_title_background'].'; color: '.$color['panel_title_textcolor'].'">
+                        '.lang('rules').'
+                    </TD>
+                </TR></TABLE>
+                </TD></TR>
+                    <TR><TD>'.content__get_content("rules").'</TD></TR>';
             }
+            if ($settings['registration__require_privacy_policy_acceptance']=='y') {
+                echo '<TR><TD>
+                    <TABLE width="100%" border=0 class="or_panel_title"><TR>
+                    <TD style="background: '.$color['panel_title_background'].'; color: '.$color['panel_title_textcolor'].'">
+                        '.lang('privacy_policy').'
+                    </TD>
+                    </TR></TABLE>
+                </TD></TR>
+                        <TR><TD>'.content__get_content("privacy_policy").'</TD></TR>';
+            }
+            
         } else {
             $_SESSION['rules']=true;
             redirect ("public/".thisdoc());
