360f4e1b14c850acfad4e47d1c71e3919a2a0aea
Carlo1911 <carlo.alva@pucp.pe>
2017-08-10 23:50:26 -0500

    prueba termns

--- public/terms_conditions.php
+++ public/terms_conditions.php
@@ -4,35 +4,29 @@ ob_start();
 $menu__area="terms_conditions";
 $title="terms_conditions";
 include ("header.php");
-
-
 if ($proceed) {
-    // check for rules
-    if (!isset($_SESSION['rules'])) {
-            echo '<center>
-                    <FORM action='.thisdoc().'>
-                    <TABLE class="or_panel" style="width: 80%">';
-            if ($settings['registration__require_rules_acceptance']=='y') {
-                echo '<TR><TD>
-                <TABLE width="100%" border=0 class="or_panel_title"><TR>
-                    <TD style="background: '.$color['panel_title_background'].'; color: '.$color['panel_title_textcolor'].'">
-                        '.lang('rules').'
-                    </TD>
-                </TR></TABLE>
-                </TD></TR>
-                    <TR><TD>'.content__get_content("rules").'</TD></TR>';
-            }
-            if ($settings['registration__require_privacy_policy_acceptance']=='y') {
-                echo '<TR><TD>
-                    <TABLE width="100%" border=0 class="or_panel_title"><TR>
-                    <TD style="background: '.$color['panel_title_background'].'; color: '.$color['panel_title_textcolor'].'">
-                        '.lang('privacy_policy').'
-                    </TD>
-                    </TR></TABLE>
-                </TD></TR>
-                        <TR><TD>'.content__get_content("privacy_policy").'</TD></TR>';
-            }
-        $proceed=false;
+    echo '<center>
+            <FORM action='.thisdoc().'>
+            <TABLE class="or_panel" style="width: 80%">';
+    if ($settings['registration__require_rules_acceptance']=='y') {
+        echo '<TR><TD>
+        <TABLE width="100%" border=0 class="or_panel_title"><TR>
+            <TD style="background: '.$color['panel_title_background'].'; color: '.$color['panel_title_textcolor'].'">
+                '.lang('rules').'
+            </TD>
+        </TR></TABLE>
+        </TD></TR>
+            <TR><TD>'.content__get_content("rules").'</TD></TR>';
+    }
+    if ($settings['registration__require_privacy_policy_acceptance']=='y') {
+        echo '<TR><TD>
+            <TABLE width="100%" border=0 class="or_panel_title"><TR>
+            <TD style="background: '.$color['panel_title_background'].'; color: '.$color['panel_title_textcolor'].'">
+                '.lang('privacy_policy').'
+            </TD>
+            </TR></TABLE>
+        </TD></TR>
+                <TR><TD>'.content__get_content("privacy_policy").'</TD></TR>';
     }
 }
 
